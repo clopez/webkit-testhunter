b8f79d6f04252f6e8aa0083f93c26dc3044b75d7
Diego Pino Garcia <dpino@igalia.com>
2020-04-16 14:51:15 +0200

    fetch.sh: allow one or more test-bots as argument

--- fetch.sh
+++ fetch.sh
@@ -2,26 +2,68 @@
 #  Carlos Alberto Lopez Perez <clopez@igalia.com>
 set -eu
 
+usage () {
+   local exit_code="$1"
+
+   echo "fetch.sh [BOT] ..."
+   echo "Retrieves tests data from a set of bots. In case no bot is set, data is retrieved from all bots."
+   echo ""
+   echo "ARGUMENTS:"
+   echo -e "\tBOT\tgtk-release, gtk-debug, wpe-release, wpe-debug, gtk-release-old, gtk-release-wk2, gtk-release-wayland"
+
+   exit $exit_code
+}
+
 urlencode () {
 	python -c "import urllib; print urllib.quote(\"${@}\")"
 }
 webkitresultsurl="http://build.webkit.org/results"
+
 # The GTK test bot is now "GTK Linux 64-bit Release (Tests)".
 # The others were the names of this test bot in the past.
 # We fetch all the data from the actual test bot as also the past ones, to have a complete history
-webkitbots=(
-	"GTK Linux 64-bit Release"
-	"GTK Linux 64-bit Release WK2 (Tests)"
-	"GTK Linux 64-bit Release (Tests)"
-	"GTK Linux 64-bit Debug (Tests)"
-	"GTK Linux 64-bit Release Wayland (Tests)"
-	"WPE Linux 64-bit Release (Tests)"
-	"WPE Linux 64-bit Debug (Tests)"
+declare -A webkitbots_map
+webkitbots_map=(
+   ["gtk-release-old"]="GTK Linux 64-bit Release"
+   ["gtk-release-wk2"]="GTK Linux 64-bit Release WK2 (Tests)"
+   ["gtk-release"]="GTK Linux 64-bit Release (Tests)"
+   ["gtk-debug"]="GTK Linux 64-bit Debug (Tests)"
+   ["gtk-release-wayland"]="GTK Linux 64-bit Release Wayland (Tests)"
+   ["wpe-release"]="WPE Linux 64-bit Release (Tests)"
+   ["wpe-debug"]="WPE Linux 64-bit Debug (Tests)"
 )
+webkitbots_keys=()
+for each in "${!webkitbots_map[@]}"; do
+   webkitbots_keys+=( "$each" )
+done
+webkitbots_values=()
+for each in "${webkitbots_map[@]}"; do
+   webkitbots_values+=( "$each" )
+done
+
+if [[ $# -gt 0 ]]; then
+   # Check any of the arguments is help.
+   for arg in "$@"; do
+      if [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
+         usage 0
+      fi
+   done
+
+   # Parse arguments.
+   webkitbots_values=()
+   for arg in "$@"; do
+      if [[ ! " ${webkitbots_keys[@]} " =~ " $arg " ]]; then
+         echo "Error: Invalid argument: '$arg'"
+         exit 1
+      fi
+      webkitbots_values+=( "${webkitbots_map[$arg]}" )
+   done
+fi
+
 alreadytried=".cache_already_tried"
 islegacybot=".is_legacy_bot"
 cd jsonresults
-for webkitbot in "${webkitbots[@]}"; do
+for webkitbot in "${webkitbots_values[@]}"; do
 	test -d "${webkitbot}" || mkdir "${webkitbot}"
 	cd "${webkitbot}"
 	if test -f "${islegacybot}"; then
