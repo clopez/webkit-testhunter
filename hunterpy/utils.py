78fa00fd297b17f04ffe1d2cb342127d2f87668f
Carlos Alberto Lopez Perez <clopez@igalia.com>
2025-12-03 12:28:00 +0000

    Show flakiness_factor data also on wktesthunter results:
     * move flakiness_factor calculations to utils.py to share the
       code between both scripts.
    
     * improve a bit the description of what this is about.

--- hunterpy/utils.py
+++ hunterpy/utils.py
@@ -24,3 +24,25 @@ class OptionalColorText():
 
 def sigterm_this_process(signum, frame):
     os.kill(os.getpid(), signal.SIGTERM)
+
+
+def get_percent(part, total):
+    return float(100) * float(part) / float(total)
+
+
+def get_percent_str(part, total):
+    percent_part = get_percent(part, total)
+    return "%d%%" % int(percent_part) if percent_part.is_integer() else "%.1f%%" % percent_part
+
+
+def calculate_flakiness_factor(results_list):
+    flakiness_factor = 0
+    prev_result = results_list[0]
+    for result in results_list:
+        # If in a run it got two results means it was already flaky in that run
+        if ' ' in result:
+            flakiness_factor += 1
+        elif result != prev_result:
+            flakiness_factor += 1
+        prev_result = result
+    return get_percent(flakiness_factor, len(results_list))
